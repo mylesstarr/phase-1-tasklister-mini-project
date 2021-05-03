65801b544d68fa7424dd8cc894dbdf4a9e86627a
Myles Starr <mylesstarr@gmail.com>
2021-05-03 14:37:26 -0400

    Lab Monday 3PM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//   console.log("The DOM has loaded");
+// }); 
 
 const elementList = document.getElementById('list')
 const inputForm = document.getElementById('create-task-form')
@@ -5,13 +8,13 @@ const inputForm = document.getElementById('create-task-form')
 function addEventToForm(){
   inputForm.addEventListener('submit', (event) => {
     event.preventDefault()
-    console.log(event)
+    // console.log(event)
     // console.log(event.target['new-task-description'].value)
     let p = document.createElement('p')
     p.textContent = event.target['new-task-description'].value
-    console.log(p)
-    const styleSelection = document.getElementById("high")
-      console.log(styleSelection.innerText)
+    // console.log(p)
+    const styleSelection = document.getElementsByClassName("priority-menu")
+      console.log(styleSelection)
     if (styleSelection.innerText === "High") {p.style.color = "red"}
     
     elementList.appendChild(p)
@@ -27,13 +30,12 @@ function deleteEvent(node){
   deleteEntry.innerText = " Delete"
   deleteEntry.style.color = "red"
   deleteEntry.style.fontWeight = "bold"
-  deleteEntry.style
   node.appendChild(deleteEntry)
   deleteEntry.addEventListener('click', (event) => {
     console.log("clicked")
     // console.log(document.getElementsByTagName("p"))
-    const itemToDelete = document.getElementsByTagName("p")
-    console.log(itemToDelete)
+    // const itemToDelete = document.getElementsByTagName("p")
+    // console.log(itemToDelete)
     node.remove()
     // itemToDelete.remove() })
 })
